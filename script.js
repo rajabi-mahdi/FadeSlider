05c6eb02365154b952fa3190d681b187bc1b7f55
mahdi <mahdii858587@gmmail.com>
2023-12-03 18:19:55 +0330

    FadeSlider

---
+++ script.js
@@ -0,0 +1,44 @@
+$(function (){
+    $(SliderOptions.sliderContainer).append("<img src="+SliderOptions.imageRoot+'/2.jpg'+" />")
+    $(SliderOptions.sliderContainer).append("<img src="+SliderOptions.imageRoot+'/1.jpg'+" />")
+    var style = `       
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        ${SliderOptions.sliderContainer}{
+            width: 50%;
+            height: 450px;
+            margin: 50px auto;
+            box-shadow: 2px 2px 10px #0711c9;
+            position: relative;
+        }
+        ${SliderOptions.sliderContainer} img{
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+        }`;
+    if ($('style').length == 0){
+        $('head').prepend(style)
+    }else{
+        $('style').text($('style').text() + style);
+    }
+    var counter = 3;
+    fadeSlide()
+    function fadeSlide() {
+        $(SliderOptions.sliderContainer + ' img:nth-child(2)').fadeOut(2000, function () {
+            $(this).remove();
+          ($(SliderOptions.sliderContainer + ' img:first-child').before("<img alt='image' src="+SliderOptions.imageRoot+'/'+counter+'.jpg'+" />"))
+            counter++;
+            fadeSlide()
+            $(SliderOptions.sliderContainer + ' img').on("error",function (){
+                $(this).attr('src',SliderOptions.imageRoot+'/1.jpg')
+                counter = 2;
+            })
+
+        });
+    }
+})
